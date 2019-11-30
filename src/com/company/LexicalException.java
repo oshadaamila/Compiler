b5de42f290cd690f057721311e024b44c76d7721
Oshada <oshadaamila.15@cse.mrt.ac.lk>
2019-11-30 12:14:16 +0530

    first commit

---
+++ src/com/company/LexicalException.java
@@ -0,0 +1,12 @@
+package com.company;
+
+/**
+ * Created by Oshada on 2019-11-30.
+ */
+public class LexicalException extends Exception
+{
+    public LexicalException( String e )
+    {
+        super( e );
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+/**
+ * Created by Oshada on 2019-11-30.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Test");
+    }
+}
