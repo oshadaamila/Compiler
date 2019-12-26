f986e395de7abe6b260abded38a32ef5bfc61cda
Oshada <oshadaamila.15@cse.mrt.ac.lk>
2019-12-26 21:36:44 +0530

    first commit on winzig

--- src/com/company/Parser.java
+++ src/com/company/Parser.java
@@ -33,58 +33,58 @@ public class Parser {
 
     }
 
-    private void start() throws IOException, LexicalException, ParseException {
-        int n = 1;
-        type = currentToken.getType();
-        switch (type) {
-            case BEGIN:
-                read(Token.TYPE.BEGIN);
-                start();
-                read(Token.TYPE.END);
-                //add build tree function here
-                break;
-            case IDENTIFIER:
-                read(Token.TYPE.IDENTIFIER);
-                read(Token.TYPE.ASSIGNMENT);
-                E();
-                read(Token.TYPE.SEMICOLON);
-                break;
-            default:
-                throw new ParseException("Cannot Parse");
-
-        }
-
-    }
-
-    private void E() throws IOException, LexicalException, ParseException {
-        T();
-        while (type == Token.TYPE.PLUS) {
-            read(Token.TYPE.PLUS);
-            T();
-        }
-    }
-
-    private void T() throws IOException, LexicalException, ParseException {
-        P();
-        if (type == Token.TYPE.MULTIPLY) {
-            read(Token.TYPE.MULTIPLY);
-            T();
-        }
-    }
-
-    private void P() throws IOException, LexicalException, ParseException {
-        switch (type) {
-            case OPEN_PAREN:
-                read(Token.TYPE.OPEN_PAREN);
-                E();
-                read(Token.TYPE.CLOSED_PAREN);
-                break;
-            case IDENTIFIER:
-                read(Token.TYPE.IDENTIFIER);
-                read(Token.TYPE.END);
-                break;
-        }
-    }
+//    private void start() throws IOException, LexicalException, ParseException {
+//        int n = 1;
+//        type = currentToken.getType();
+//        switch (type) {
+//            case BEGIN:
+//                read(Token.TYPE.BEGIN);
+//                start();
+//                read(Token.TYPE.END);
+//                //add build tree function here
+//                break;
+//            case IDENTIFIER:
+//                read(Token.TYPE.IDENTIFIER);
+//                read(Token.TYPE.ASSIGNMENT);
+//                E();
+//                read(Token.TYPE.SEMICOLON);
+//                break;
+//            default:
+//                throw new ParseException("Cannot Parse");
+//
+//        }
+//
+//    }
+//
+//    private void E() throws IOException, LexicalException, ParseException {
+//        T();
+//        while (type == Token.TYPE.PLUS) {
+//            read(Token.TYPE.PLUS);
+//            T();
+//        }
+//    }
+//
+//    private void T() throws IOException, LexicalException, ParseException {
+//        P();
+//        if (type == Token.TYPE.MULTIPLY) {
+//            read(Token.TYPE.MULTIPLY);
+//            T();
+//        }
+//    }
+//
+//    private void P() throws IOException, LexicalException, ParseException {
+//        switch (type) {
+//            case OPEN_PAREN:
+//                read(Token.TYPE.OPEN_PAREN);
+//                E();
+//                read(Token.TYPE.CLOSED_PAREN);
+//                break;
+//            case IDENTIFIER:
+//                read(Token.TYPE.IDENTIFIER);
+//                read(Token.TYPE.END);
+//                break;
+//        }
+//    }
 
 
     private void read(Token.TYPE type) throws IOException, LexicalException, ParseException {
